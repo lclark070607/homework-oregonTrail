841d3d861381223e94dccc2b46f220bc0ea3cfdf
Louise Clark <lclark070607@gmail.com>
2017-02-05 11:44:08 -0500

    edits

--- oregon.js
+++ oregon.js
@@ -1,6 +1,6 @@
 function makeTraveler(name) {
     return {
-        amount: 50,
+        amount: 45,
         name: name,
         isHealthy: true,
     };
@@ -20,7 +20,7 @@ function makeWagon(capacity) {
 
 }
 let ride = makeWagon(5);
-console.log(ride);
+console.log(ride)
 
 
 function hunt(traveler) {
@@ -38,70 +38,67 @@ hunt(traveler2);
 console.log(traveler2);
 
 
+function eat(traveler) {
+    traveler.amount = (traveler.amount - 20);
+    if (traveler.amount < 20) {
+        traveler.isHealthy = false;
+    };
+};
 
-    function eat(traveler) {
-       //Consumes 20 of the traveler's food. 
-       //If the traveler doesn't have 20 food, the traveler is no longer healthy.
-            
-            
-            traveler.amount = (traveler.amount - 20);
-            if (traveler.amount < 20); {
-              traveler.isHealthy = false;
-          };
-          
-      } 
-      eat(traveler);
-      console.log(traveler)
-      eat(traveler2);
-      console.log(traveler2);
+eat(traveler);
+console.log(traveler)
+eat(traveler2);
+console.log(traveler2)
 
-      
-      function join(wagon, traveler) {
-        // Add the traveler to the wagon if there is space. 
-         // If there is not enough capacity, don't add them.
-         
-            let total = 0;
+// console.log(ride.passengers.length)
+function join(wagon, peep) {
+    // Add the traveler to the wagon if there is space. 
+    // If there is not enough capacity, don't add them.
+    
+    for (i = 0; i < wagon.passengers.length; i++) {
 
-            for (i = 0; i < wagon.passengers.length; i++ ) { 
-             
-             if (wagon.passengers.length <= 4) 
-                wagon.passengers.push(traveler2)
-                wagon.passengers.push
-         }
-    }
-
-       let success = join(3, traveler2);
-       console.log(success);
-
-      function quarantine(wagon) {
-        // Return true if there is at least one unhealthy person in the wagon. 
-        // Return false if not.
-        // wagon is an array
-
-          if ((traveler.isHealthy = false) || (traveler2.isHealthy = false)) {
-
-          }
-
-      }
-        function food(wagon) {
-            let total = 0;
-            total = (total + wagon.traveler.amount + wagon.traveler2.amount);
+        if (wagon.passengers.length <= 4) {
+           wagon.passengers.push(peep)
         }
-        console.log(total);
-            //Return the total amount of food among all occupants of the wagon.
+    }
+    return wagon.passengers.push(peep);
+};
+
+join(ride, 'henrietta');
+join(ride, 'juan');
+console.log(ride)
 
 
 
-//Code Should Work:
-// let wagon = makeWagon(5);
-// let traveler = makeTraveler('Henrietta');
-// let traveler2 = makeTraveler('Juan');
+//       function quarantine(wagon) {
+//         // Return true if there is at least one unhealthy person in the wagon. 
+//         // Return false if not.
+//         // wagon is an array
 
-//hunt(traveler); // maybe get more food
-//eat(traveler2);
-//eat(traveler2); // juan is hungry
-//join(wagon, traveler);
-//join(wagon, traveler2);
+//           if ((traveler.isHealthy = false) || (traveler2.isHealthy = false)) {
 
-//console.log(quarantine(wagon)); // print true if someone is sick, false otherwise
-//console.log(food(wagon)); // print juan's food + henrietta's food
+//           }
+
+//       }
+//         function food(wagon) {
+//             let total = 0;
+//             total = (total + wagon.traveler.amount + wagon.traveler2.amount);
+//         }
+//         console.log(total);
+//             //Return the total amount of food among all occupants of the wagon.
+
+
+
+// //Code Should Work:
+// // let wagon = makeWagon(5);
+// // let traveler = makeTraveler('Henrietta');
+// // let traveler2 = makeTraveler('Juan');
+
+// //hunt(traveler); // maybe get more food
+// //eat(traveler2);
+// //eat(traveler2); // juan is hungry
+// //join(wagon, traveler);
+// //join(wagon, traveler2);
+
+// //console.log(quarantine(wagon)); // print true if someone is sick, false otherwise
+// //console.log(food(wagon)); // print juan's food + henrietta's food
